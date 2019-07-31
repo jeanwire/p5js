ee1a78568af17266009a98082e5c4e7971eefaf3
Glitch (paint-dentist-1) <none>
2019-07-31 14:49:13 +0000

    first commit

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 /*global abs,angleMode,append,background,beginShape,bezier,box,camera,ceil,CENTER,color,cone,cos,createCanvas,createCanvas,createGraphics,curveVertex,cylinder,DEGREES,displayHeight,displayWidth,dist,div,DOWN_ARROW,ellipse,endShape,fill,floor,frameCount,frameRate,height,image,key,keyCode,keyIsDown,keyIsPressed,keyIsPressed,keyPressed,LEFT,LEFT_ARROW,lerpColor,line,loadImage,loadJSON,loadSound,map,mouseIsPressed,mouseX,mouseY,noFill,noLoop,normalMaterial,noStroke,p5,plane,point,pointLight,pop,push,push,RADIANS,radians,random,rect,resizeCanvas,resizeCanvas,RIGHT,RIGHT_ARROW,rotate,rotateX,rotateY,rotateZ,round,round,scale,shuffle,sin,sphere,stroke,strokeWeight,text,textAlign,textFont,textSize,texture,textWidth,torus,translate,triangle,UP_ARROW,WEBGL,width,windowHeight,windowHeight,windowWidth,world */
+// When an empty space on the canvas is clicked, a circle is generated
+// When a circle is clicked, its speed increases
 
 const circles = [];
 const canvasWidth = 400;
@@ -40,7 +42,6 @@ function draw() {
       let circle = newCircle(mouseX, mouseY);
       
       circles.push(circle);
-
     }
   }
   else {
